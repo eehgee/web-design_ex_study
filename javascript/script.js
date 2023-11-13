e034a25e0ba290e71d40b726ee5a7b52a742797e
Eunju Kim <dfiow@naver.com>
2023-11-13 20:16:27 +0900

    weba

--- javascript/script.js
+++ javascript/script.js
@@ -28,20 +28,6 @@ $(function(){
     }, 3000);
     // 슬라이드 끝
 
-    // 탭 시작
-    let tabBtn = $(".tab-btn > ul > li");
-    let tabCont = $(".tab-cont > div");
-
-    tabCont.hide().eq(0).show();
-
-    tabBtn.click(function(){
-        const index = $(this).index();
-
-        $(this).addClass("active").siblings().removeClass("active");
-        tabCont.eq(index).show().siblings().hide();
-    });
-    // 탭 끝
-
     // 모달 레이어 팝업창 시작
     $(".popup-btn").click(function(){
         $(".popup-review").show();
